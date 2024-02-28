d550d4ad93aa7dae59335b2fa9eb9d3bcd9ec2d5
CledisonSantos <scledison050@gmail.com>
2024-02-28 11:33:48 -0300

    inicio do projeto

---
+++ class/Sql.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+classe extende da classe PDO para utilizar 
+dos metodos ja existentes com excute,bindParam,prepare... etc
+*/
+
+class Sql extends PDO
+{
+
+  private $conn;
+
+  //metodo de conexão do banco de dados
+  public function __construct()
+  {
+
+    $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","");
+  }
+  //metodo  recebe os parametros 
+  private function setParams($statment, $parameters = array())
+  {
+
+    foreach ($parameters as $key => $value) {
+
+      $this->setParam($key, $value);
+    }
+  }
+
+  private function setParam($statment, $key, $value)
+  {
+
+    $statment->bindParam($key, $value);
+
+  }
+
+  //metodo que executa  querys no banco de dados
+  public function run($rawQuery, $params = array())
+  {
+
+    $stmt = $this->conn->prepare($rawQuery);
+
+    $this->setParams($stmt, $params);
+    $stmt->execute();
+    return $stmt;
+  }
+
+  //metodo seclect
+  public function select($rawQuery, $params = array()):array
+  {
+
+    $stmt = $this->run($rawQuery,$params);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
+}
